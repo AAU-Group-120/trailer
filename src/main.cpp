cb57db7110239b527a54c97b2495ce1f418b611e
Vilhelm Vobbe <vilhelmvobbe@gmail.com>
2024-10-29 10:22:35 +0100

    Fix: voltage and current calc

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,11 @@
 #include "stdint.h"
 
 const int hallEffektPin = 36;
-const int voltageValue = 12;
+const int voltageValueWire = 12;
 uint32_t hallEffektValue;
 float currentValue;
 float resistensValue;
+float voltageValue;
 
 void setup() {
   Serial.begin(115200);
@@ -17,10 +18,12 @@ void loop() {
   hallEffektValue = analogRead(hallEffektPin);
   Serial.println(hallEffektValue, DEC); // print as an ASCII-encoded decimal
   delay(10);
-  currentValue = map(hallEffektValue, 1.5 , 3.5 , -5 , 5); // Mapper værdien til bage til ampere
+  voltageValue = hallEffektValue*5/1023;
+  currentValue = (voltageValue-2.5)/0.185;
+  //currentValue = map(hallEffektValue, 1.5 , 3.5 , -5 , 5); // Mapper værdien til bage til ampere
   Serial.println(currentValue, DEC); // print as an ASCII-encoded decimal
   delay(10);
-  resistensValue = voltageValue/currentValue; // beregner resistencen
+  resistensValue = voltageValueWire/currentValue; // beregner resistencen
   Serial.println(resistensValue, DEC); // print as an ASCII-encoded decimal
   delay(10);
 }
