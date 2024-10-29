658967a03e1e5a5f970cda713daa1d1b2445f10f
Vilhelm Vobbe <vilhelmvobbe@gmail.com>
2024-10-29 10:27:38 +0100

    fix: sidste commit mb

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,23 @@ void setup() {
 }
 
 void loop() {
+  //analog read resultat print
   hallEffektValue = analogRead(hallEffektPin);
   Serial.println(hallEffektValue, DEC); // print as an ASCII-encoded decimal
   delay(10);
+
+  //voltage in hall effekt pin
   voltageValue = hallEffektValue*5/1023;
+  Serial.println(voltageValue, DEC); // print as an ASCII-encoded decimal
+  delay(10);
+
+  //calculating current value and printing
   currentValue = (voltageValue-2.5)/0.185;
   //currentValue = map(hallEffektValue, 1.5 , 3.5 , -5 , 5); // Mapper værdien til bage til ampere
   Serial.println(currentValue, DEC); // print as an ASCII-encoded decimal
   delay(10);
+
+  //calculating ressistence value
   resistensValue = voltageValueWire/currentValue; // beregner resistencen
   Serial.println(resistensValue, DEC); // print as an ASCII-encoded decimal
   delay(10);
