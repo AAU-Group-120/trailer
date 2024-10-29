ed791399ebb112983868ba947b8983063552ada5
Vilhelm Vobbe <vilhelmvobbe@gmail.com>
2024-10-29 08:52:11 +0100

    Feat: values of wire using halleffekt

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 #include "HardwareSerial.h"
 #include "stdint.h"
 
-const int currentPin = 36;
-uint32_t currentValue;
+const int hallEffektPin = 36;
+const int voltageValue = 12;
+uint32_t hallEffektValue;
+float currentValue;
+float resistensValue;
 
 void setup() {
   Serial.begin(115200);
@@ -11,7 +14,13 @@ void setup() {
 }
 
 void loop() {
-  currentValue = analogRead(currentPin);
+  hallEffektValue = analogRead(hallEffektPin);
+  Serial.println(hallEffektValue, DEC); // print as an ASCII-encoded decimal
+  delay(10);
+  currentValue = map(hallEffektValue, 1.5 , 3.5 , -5 , 5); // Mapper værdien til bage til ampere
   Serial.println(currentValue, DEC); // print as an ASCII-encoded decimal
+  delay(10);
+  resistensValue = voltageValue/currentValue; // beregner resistencen
+  Serial.println(resistensValue, DEC); // print as an ASCII-encoded decimal
   delay(10);
 }
