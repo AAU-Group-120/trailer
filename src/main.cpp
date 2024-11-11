5626bca7fc416b1f18dfd16925d090447bbdd6ea
Thorup08 <NH55GN@student.aau.dk>
2024-11-11 15:14:17 +0100

    Update main.cpp
    Sidste push for i dag

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,9 @@
 // Overvej at lav en address, ellers vil koden kunne spores til alle bluetooth enheder og ikke kun det ønskede mål
 // For nu, nøjes vi dog med, at se det som en ekstra indskydning, men holder reciveren til at finde transmitteren
 
-char chartosend = '1';
-
-int fejl = 2;
 
 
-
+unsigned char fejl = 0b01000000;
 
 BluetoothSerial SerialBT;     // Create a Bluetooth Serial object
 void setup() {
@@ -19,9 +16,9 @@ void setup() {
   SerialBT.begin("ESP32_Transmitter"); // Set Bluetooth name to "ESP32_Transmitter"
   Serial.println("Bluetooth Device is ready to pair");
 }
-
+/*
 void message(int fejl){
-  switch (fejl)
+   switch (fejl)
   {
   case 1: SerialBT.print("fejl 1"); Serial.println("fejl 1"); break; //Fejl 1 is being sent via bluetooth
   case 2: SerialBT.print("fejl 2"); Serial.println("fejl 2"); break;
@@ -30,10 +27,12 @@ void message(int fejl){
     break;
   }
 }
-
+*/
 
 void loop() {
-  message(fejl);
+  //message(fejl);
+  SerialBT.print(fejl);
+  Serial.println(fejl);
   delay(5000); // Wait 5 seconds before sending again
 
 }
