fed2c26b450acc55a1ac32f688af9896aad5433d
Thorup08 <NH55GN@student.aau.dk>
2024-11-11 12:19:38 +0100

    Update main.cpp
    Bluetooth Transmitter

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,19 @@
 #include "BluetoothSerial.h"
 
 
+char chartosend = '1';
+
+BluetoothSerial SerialBT;     // Create a Bluetooth Serial object
+
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(115200);        // Initialize serial monitor
+  SerialBT.begin("ESP32_Transmitter"); // Set Bluetooth name to "ESP32_Transmitter"
+  Serial.println("Bluetooth Device is ready to pair");
 }
-
-
 
 void loop() {
-
+  SerialBT.print(chartosend); // Send "Hello, World!" over Bluetooth
+  Serial.println(chartosend); // Print confirmation to Serial Monitor
+  delay(5000); // Wait 5 seconds before sending again
 }
+
